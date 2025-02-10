26b0682b30191781ba5cf3c6e9dccc7eb2e3090b
sasekanimaluleke92@gmail.com <sasekanimaluleke92@gmail.com>
2025-02-10 16:08:00 +0200

    insert location

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,7 +11,7 @@ export class NavComponent {
   home= 'Home'
 
   navbarpro = [
-    'HOME' , 'About' , 'Services' , 'Contct', 'Testimonial' , 'FAQ'
+    'HOME' , 'About' , 'Services' , 'Contct', 'Testimonial' ,'Location'
   ]
 
 }
